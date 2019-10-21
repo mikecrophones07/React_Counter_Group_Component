83e0db8837efa13992aa4e322f586654617317ee
MIKE ANTHONY ECHON (SHP-IRIS-ISD-OOCLL/MNL) <ECHONMI@OOCL.COM>
2019-10-21 16:06:18 +0800

    add function for button and onchange for input and button

--- react-counter/src/components/CounterGroup/CounterGroup.js
+++ react-counter/src/components/CounterGroup/CounterGroup.js
@@ -4,18 +4,32 @@ import Counter from '../Counter/Counter'
 class CounterGroup extends React.Component {
   constructor(props){
     super(props);
+    this.state ={
+        counterGroup: this.props.defaultCounter,
+        inputValue:''
+    }
+  }
 
+  handleInputChange = (event) => {
+    const re = /^[0-9\b]+$/;
+    if (event.target.value === '' || re.test(event.target.value)) {
+      this.setState({inputValue: event.target.value})
+    }
+  }
+
+  regenerateCounters = () => {
+      this.setState({counterGroup: this.state.inputValue})
   }
   
   render(){
-    let element = Array.apply(null, {length: this.props.defaultCounter}).map(element => {
+    let element = Array.apply(null, {length: this.state.counterGroup}).map(element => {
         return (<Counter/>);
     })
     return (
       <div>
-          <input type = "text"/>
-          <button>Regenerate Counter</button>
-          <span>Sum: 0</span>
+          <input type = "text" value={this.state.inputValue} onChange={this.handleInputChange}/>
+          <button onClick={this.regenerateCounters}>Regenerate Counter</button>
+          <span>Sum: 3</span>
           {element}
       </div>
     );
