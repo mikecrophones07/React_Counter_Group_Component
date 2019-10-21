4e333825bfd34467539d6626a9e5fc2021de33c8
MIKE ANTHONY ECHON (SHP-IRIS-ISD-OOCLL/MNL) <ECHONMI@OOCL.COM>
2019-10-21 15:17:45 +0800

    elements implimented without design

--- react-counter/src/App.js
+++ react-counter/src/App.js
@@ -2,14 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {value: ''};
+    this.onChange = this.onChange.bind(this)
+ }
+ 
+ onChange(e){
+    const re = /^[0-9\b]+$/;
+    if (e.target.value === '' || re.test(e.target.value)) {
+       this.setState({value: e.target.value})
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <input className="TextBox-Prop" value={this.state.value} onChange={this.onChange}/>
+          <button>Regenerate Counter</button>
+          <p>Sum: 0</p>
+          <p>
+            <button>+</button>
+            0
+            <button>-</button>
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
